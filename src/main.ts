d667ea501aaa21d8668460b8a71c393353b9b204
shaha <ayubjonovshakhzod01@gmail.com>
2025-07-31 13:40:57 +0500

    changed port

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3303;
 
   await app.listen(port);
   console.log(`🚀 NestJS Telegram Bot is running on port ${port}`);
